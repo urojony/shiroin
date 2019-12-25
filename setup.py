0425a06adb40ab0bd78db610c6c31a3b3a9a422a
urojony <grzes-a1@o2.pl>
2019-12-25 08:28:24 +0100

    add dependencies

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
+    install_requires=['scipy>=0.15','sympy'],
     classifiers=[
     "License :: OSI Approved :: BSD License",
     "Operating System :: OS Independent",
